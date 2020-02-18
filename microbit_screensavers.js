357a8479859951ef99b74b9769682c9bc093de19
mmoham14 <60205490+mmoham14@users.noreply.github.com>
2020-02-17 19:16:48 -0700

    Update microbit_screensavers.js

--- microbit_screensavers.js
+++ microbit_screensavers.js
@@ -1,45 +1,197 @@
-let item = game.createSprite(2, 2);
-let awakeMode: boolean = true
+let sleep: boolean = true
+let motion: Gesture = Gesture.Shake
 
-input.onButtonPressed(Button.A, function () {
-    awakeMode = true
+/*First*/input.onButtonPressed(Button.A, function () {
+    sleep = true
 })
-input.onButtonPressed(Button.B, function () {
-    awakeMode = false
+/*Second*/input.onButtonPressed(Button.B, function () {
+    sleep = false
 })
 
-function screensaver1() {
-    basic.showString("|/-\|")
+/*1*/input.onGesture(Gesture.Shake, function () {
+    motion = Gesture.Shake
+})
+
+/*2*/input.onGesture(Gesture.LogoDown, function () {
+    motion = Gesture.LogoDown
+})
+
+/*3*/input.onGesture(Gesture.LogoUp, function () {
+    motion = Gesture.LogoUp
+})
+
+/*4*/input.onGesture(Gesture.TiltLeft, function () {
+    motion = Gesture.TiltLeft
+})
+
+/*5*/input.onGesture(Gesture.TiltRight, function () {
+    motion = Gesture.TiltRight
+})
+
+/*a*/function saver1() {
+    basic.showIcon(IconNames.Heart)
+    basic.pause(100)
+    basic.clearScreen()
 }
 
-function screensaver2() {
-    item.setX(Math.randomRange(0, 4));
-    item.setY(Math.randomRange(0, 4));
-    item.ifOnEdgeBounce();
-    item.move(1);
+/*b*/function saver2() {
+    let sprite = game.createSprite(Math.randomRange(0, 4), Math.randomRange(0, 4))
+    if (Math.randomBoolean()) {
+        sprite.turn(Direction.Right, 45)
+    } else {
+        sprite.turn(Direction.Left, 45)
+    }
+    sprite.move(2)
+    sprite.ifOnEdgeBounce()
+    basic.pause(300)
+    sprite.delete()
+    basic.clearScreen()
 }
 
-function screensaver3() {
-    basic.showIcon(IconNames.Heart);
+/*c*/function saver3() {
+    basic.showLeds
+        (`
+    . . . . .
+    . . . . .
+    . . . . .
+    . . . . .
+    . . # . .
+    `)
+
+    //
+
+    basic.showLeds
+        (`
+    . . . . .
+    . . . . .
+    . . . . .
+    . . # . .
+    . # . # .
+    `)
+
+    //
+
+    basic.showLeds
+        (`
+    . . . . .
+    . . . . .
+    . . # . .
+    . # . # .
+    # # # # #
+    `)
+
+    //
+
+    basic.showLeds
+        (`
+    . . . . .
+    . . # . .
+    . # . # .
+    # # # # #
+    . # . # .
+    `)
+
+    //
+
+    basic.showLeds
+        (`
+    . . # . .
+    . # . # .
+    # # # # #
+    . # . # .
+    . # . # .
+    `)
+
+
+
+    basic.showLeds
+        (`
+    . # . # .
+    # # # # #
+    . # . # .
+    . # . # .
+    . . . . .
+    `)
+
+    //
+
+    basic.showLeds
+        (`
+    # # # # #
+    . # . # .
+    . # . # .
+    . . . . .
+    # . . # .
+    `)
+
+    //
+
+    basic.showLeds
+        (`
+    . # . # .
+    . # . # .
+    . . . . .
+    . # . # .
+    . # . . #
+    `)
+
+    //
+
+    basic.showLeds
+        (`
+    . # . # .
+    . . . . .
+    # . . # .
+    . # # . .
+    # . . # #
+    `)
+
+
+
+    basic.showLeds
+        (`
+    . . . . .
+    # . # . .
+    . # . # .
+    . . # . .
+    # . . . #
+    `)
+    basic.clearScreen()
+    basic.pause(1000)
 }
 
-function screensaver4() {
-    basic.showString("*.*.*.*.*")
+/*d*/function saver4() {
+    basic.showString("DND!!")
 }
-function screensaver5() {
-    basic.showString("-_-_-_-_-")
+
+/*e*/function saver5() {
+    basic.showIcon(IconNames.Skull)
+    basic.clearScreen()
+    basic.pause(100)
 }
 
 basic.forever(function () {
-    if (awakeMode) {
-        basic.showString("Hello")
+    if (sleep) {
+        if (motion == Gesture.Shake) {
+            saver1()
+        }
+        else if (motion == Gesture.LogoDown) {
+            saver2()
+        }
+        else if (motion == Gesture.LogoUp) {
+            saver3()
+        }
+        else if (motion == Gesture.TiltLeft) {
+            saver4()
+        }
+        else if (motion == Gesture.TiltRight) {
+            saver5()
+        }
+        else { // default
+            saver1()
+        }
     }
     else {
-        basic.showString("Sleep")
-        screensaver1();
-        screensaver2();
-        screensaver3();
-        screensaver4();
-        screensaver5();
+        basic.showString("Working")
     }
 })
